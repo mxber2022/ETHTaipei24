124933bee5c0a7b01ee5aae51e0f2e0a8c0eaa49
mxber2022 <mxber2022@gmail.com>
2024-03-23 13:53:27 +0800

    smart contract

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ETHTaipei24
  
+TEST_API_KEY:d6aff0ec0be5bc2de6de50b937af9da4:d226f701881c00faeaafb1570d7404c7
+
+RestrictedTEST_API_KEY:586b38e3a9f9489b73bea451063fc4f7:dd8ccbd3d6dfb02739a6b010ef34d6a9
---
+++ smartcontract/Bet.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+contract Bet {
+    address public manager;
+    address[] public players;
+    mapping(address => uint256) public bets;
+
+    IERC20 public usdc; // USDC token contract address
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only the manager can call this function");
+        _;
+    }
+
+    constructor(address _usdcAddress) {
+        manager = msg.sender;
+        usdc = IERC20(_usdcAddress); // Initialize USDC token contract
+    }
+
+    function enter(uint256 amount) public {
+        require(amount > 5, "Amount must be greater than 0");
+        require(usdc.allowance(msg.sender, address(this)) >= amount, "You must approve the transfer first");
+
+        players.push(msg.sender);
+        bets[msg.sender] = amount;
+
+        // Transfer USDC tokens from sender to this contract
+        bool success = usdc.transferFrom(msg.sender, address(this), amount);
+        require(success, "USDC transfer failed");
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+
+    function getPot() public view returns (uint256) {
+        uint256 pot = 0;
+        for (uint256 i = 0; i < players.length; i++) {
+            pot += bets[players[i]];
+        }
+        return pot;
+    }
+
+    function pickWinner() public onlyManager {
+        require(players.length > 0, "No players in the pool");
+        // Pseudo-randomly select a winner based on the block timestamp
+        uint256 index = block.timestamp % players.length;
+        address winner = players[index];
+
+        // Transfer the entire pot to the winner
+        uint256 pot = getPot();
+        bool success = usdc.transfer(winner, pot);
+        require(success, "USDC transfer failed");
+
+        // Reset the contract for the next round
+        delete players;
+    }
+}
